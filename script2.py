66ba0f49694fa83f5d25230dcfcba49a23df433c
garsharfleming <garshar@flemingcollege.ca>
2025-01-18 14:03:05 -0500

    Modified both script files
    -Deleted print from script1.py
    -Added comment to script2.py

--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+# Print lines from the Zen of Python
 print("If the implementation is easy to explain, it may be a good idea.")
 print("Namespaces are one honking great idea -- let's do more of those!")
